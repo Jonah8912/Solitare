1f9dda1a40807cf601ff3653c932c066e38bc3df
Jonah Perez <johndoe@email.com>
2023-12-07 19:54:58 -0500

    in the beginging

---
+++ main.py
@@ -0,0 +1,15 @@
+#Jonah Perez
+#12/6/2023
+#Solitare
+
+import pygame as pg 
+import random 
+import time
+
+pg.init()
+
+while True:
+    for event in pg.event.get():
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit
